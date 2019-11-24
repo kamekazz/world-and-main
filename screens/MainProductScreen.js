4aed8e6ae487bd99c7a193dfdc71d65062141430
kamekazz <taveras78@gmail.com>
2019-11-24 15:15:22 -0500

    u

--- screens/MainProductScreen.js
+++ screens/MainProductScreen.js
@@ -37,7 +37,7 @@ const ButtonEl = ({}) => {
             fontSize: 20
           }}
         >
-          Order Product
+          Order Product !!
         </Text>
       </View>
     </TouchableOpacity>
